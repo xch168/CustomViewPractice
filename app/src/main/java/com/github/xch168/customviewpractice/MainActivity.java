d468071021a7621f5fda105bb39e8f161062dc56
徐灿辉 <xucanhui@4399inc.com>
2017-08-08 12:55:20 +0800

    init project

---
+++ app/src/main/java/com/github/xch168/customviewpractice/MainActivity.java
@@ -0,0 +1,15 @@
+package com.github.xch168.customviewpractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
